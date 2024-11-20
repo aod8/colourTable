8abc4925f56431c6e3a9855a414a21251560b9b3
aidandodonovan <122410952@umail.ucc.ie>
2024-11-20 17:11:01 +0000

    Refactored and Included Javadoc for clarity.

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -5,15 +5,34 @@ public class ColourTable {
 
     private final int paletteSize;
 
+    /**
+     * Constructs a ColourTable with a specified palette size
+     * @param paletteSize The number of colours in the palette. Must be greater than one and be a power of 2.
+     * @throws IllegalArgumentException if the palette size is not valid.
+     */
+
+
     public ColourTable(int paletteSize) {
-        if (paletteSize <= 1 || (paletteSize & (paletteSize - 1)) != 0) {
-            throw new IllegalArgumentException("Size must be a power of two and greater than 1.");
-        }
+        validatePaletteSize(paletteSize);
         this.paletteSize = paletteSize;
     }
 
+    /**
+     * Returns the size of the colour palette.
+     * @return The palette size.
+     */
     public int getPaletteSize() {
         return paletteSize;
+    }
+    /**
+     * Validates that the given palette size is a power of two and greater than 1.
+     * @param size The palette size to validate
+     * @throws IllegalArgumentException if the size is invalid.
+     */
+    private void validatePaletteSize(int size) {
+        if (size <= 1 || (size & (size - 1)) != 0) {
+            throw new IllegalArgumentException("Palette size must be a power of two and greater than 1.");
+        }
     }
 }
 
