cea77ac2bbcacbba17a746f66dec4aa7e3957def
aidandodonovan <122410952@umail.ucc.ie>
2024-11-20 17:00:06 +0000

    Implement ColourTable Constructor to pass size validation tests

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -2,6 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ColourTable {
+
+    private final int paletteSize;
+
+    public ColourTable(int paletteSize) {
+        if (paletteSize <= 1 || (paletteSize & (paletteSize - 1)) != 0) {
+            throw new IllegalArgumentException("Size must be a power of two and greater than 1.");
+        }
+        this.paletteSize = paletteSize;
+    }
+
+    public int getPaletteSize() {
+        return paletteSize;
+    }
 }
 
 
